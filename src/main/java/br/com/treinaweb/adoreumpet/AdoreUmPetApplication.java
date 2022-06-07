08d7ebbca9d55184b6be62a363c914471f163738
rickcunha05 <henrique.cunha5@outlook.com>
2022-06-07 09:24:53 -0300

    commit on master

---
+++ src/main/java/br/com/treinaweb/adoreumpet/AdoreUmPetApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.adoreumpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoreUmPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoreUmPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinaweb/adoreumpet/AdoreUmPetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.adoreumpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoreUmPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
